d470109c213f14b2acc7732a1149244d6c16b1c5
KovTa <bigboy99899@gmail.com>
2023-01-18 20:15:45 +0100

    Finished CRUD operators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent, HeroDetailComponent, HeroesComponent } from '@app/_components';
+import { DashboardComponent, HeroDetailComponent, HeroesComponent, PageNotFoundComponent } from '@app/_components';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent }
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
